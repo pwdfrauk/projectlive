aebe41ea29e808c8515fc5bf1fe627c4c32e4d2a
Your Name <you@example.com>
2020-08-22 10:04:23 +0600

    adding live project

---
+++ resource/js/main.js
@@ -0,0 +1,12 @@
+// To apply lightbox to a group of links
+$(document).ready(function(){
+	/*mixitUp*/
+	var mixer = mixitup('.container');
+	
+	/*counter Up*/
+	$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+	});
+
+});
